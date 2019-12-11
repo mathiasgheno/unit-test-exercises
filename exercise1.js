ddb112695f1e9b19e5f8b1d66cba4af2987dcdcf
Mathias Gheno Azzolini <mathiasgheno@gmail.com>
2019-12-10 21:04:18 -0300

    Update exercise1.js

---
+++ exercise1.js
@@ -0,0 +1,31 @@
+/**
+ * Make a function.
+ * This function must receive an object of person as the first parameter,
+ * an array as the second one and a boolean as the third parameter.
+ * The object must have an "id" as property.
+ * The function must verify if the array already has an object
+ * with the same id as the object passed as parameter.
+ * If there is, the function must do nothing.
+ * If there isn't, the function must add the object in the array.
+ * But, if the third parameter is true,
+ * the function must add the object in the array without doing any validation.
+ * You need to guarantee:
+ *
+ * - When the function doesn't receive the first or the second argument,
+ * nothing must happen.
+ *
+ * - When the function receives one object and its is new,
+ * the object must be added in the array.
+ *
+ * - When the function receives one object and it is not new,
+ * the array must suffer no change.
+ *
+ * - When the third parameter receives true,
+ * the object in the array must be added without validation.
+ *
+ *
+ **/
+
+const person = {
+    id: 'xxxxsss',
+};
